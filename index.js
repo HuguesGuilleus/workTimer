9a535c6f989a4fa8db59dd9eefeb56293de180d0
hugues <ghugues@netc.fr>
2020-12-05 22:59:17 +0100

    index.js: Add Sessions

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // Octobre 2020 GUILLEUS Hugues ghugues[at]netc.fr
 
 var timer = null,
+	sessions = null,
 	out, bar;
 
 const SECOND = 1000,
@@ -8,6 +9,7 @@ const SECOND = 1000,
 	barCircumference = Math.PI * 2 * 40;
 
 document.addEventListener('DOMContentLoaded', () => {
+	sessions = Sessions.storageImport();
 	const delayWork = $('delayWork');
 	const delayExtra = $('delayExtra');
 	const delayBreak = $('delayBreak');
@@ -94,4 +96,71 @@ function wait(delay) {
 
 function $(id) {
 	return document.getElementById(id);
+}
+
+class Sessions {
+	static STORAGE = 'work-sessions';
+	static DAY = 24 * 3600 * 1000;
+	static set2Day(d) {
+		return Math.round(d / Sessions.DAY) * Sessions.DAY;
+	}
+	static now() {
+		return Sessions.set2Day(Date.now());
+	}
+	static fromJSON(j) {
+		return new Sessions(JSON.parse(j));
+	}
+	static storageImport() {
+		return new Sessions(window.localStorage.getItem(Sessions.STORAGE));
+	}
+	storageSave() {
+		window.localStorage.setItem(Sessions.STORAGE, this.toJSON());
+	}
+	constructor(obj) {
+		const nbDay = 2 * 7 + new Date().getDay();
+		let day = Sessions.now() - nbDay * Sessions.DAY;
+		this.days = new Map();
+		for (let i = 0; i < nbDay; i++) {
+			this.days.set(day, 0);
+			day += Sessions.DAY;
+		}
+
+		Object.keys(obj || {})
+			.map(k => [Sessions.set2Day(Date.parse(k)), Number(obj[k])])
+			.filter(([d, nb]) => !isNaN(d) && this.days.has(d) && !isNaN(nb))
+			.forEach(([d, nb]) => this.days.set(d, nb));
+
+		this.graph = $('graph');
+		this.graphText = $('graphText');
+		this.display();
+	}
+	toJSON() {
+		return '{' + Array.from(this.days.entries())
+			.map(([d, nb]) => `"${new Date(d).toJSON()}":${nb}`)
+			.join(',') + '}';
+	}
+	max() {
+		let m = 0;
+		this.days.forEach(nb => m = Math.max(m, nb))
+		return m;
+	}
+	addToday() {
+		const now = Sessions.now();
+		this.days.set(now, (this.days.get(now) || 0) + 1);
+		this.display();
+		this.storageSave();
+	}
+	display() {
+		Array.from(this.graph.children).forEach(item => item.remove());
+		const max = this.max() || 1;
+		for (let [k, nb] of this.days.entries()) {
+			const d = new Date(k);
+			let div = document.createElement('div');
+			this.graph.append(div);
+			div.classList.add(`graph-nb-${Math.round(nb*5/max)}`);
+			div.addEventListener('mouseover', () => {
+				this.graphText.innerText = `[${nb}] ${d.toLocaleDateString()}`;
+			});
+		}
+	}
 }
